38bc9a1cd008d8145132f4d108e32fed22214631
Matthew Clark <linux.matthew@gmail.com>
2023-06-27 18:40:11 -0400

    Custom version to add subfolder from repo

--- setup.py
+++ setup.py
@@ -1,16 +1,43 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import shutil
+import subprocess
 
+# Custom install command to clone repository and copy files
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+
+        # Clone openpose-library repository
+        clone_command = [
+            'git', 'clone', 'https://github.com/tranzmatt/openpose-library.git'
+        ]
+        subprocess.run(clone_command)
+
+        # Copy openpose-library files to the package directory
+        source_dir = 'openpose-library'
+        target_dir = os.path.join(self.install_lib, 'openpose')
+
+        os.makedirs(target_dir, exist_ok=True)
+        for filename in ['body.py', 'model.py', 'util.py']:
+            source_file = os.path.join(source_dir, filename)
+            target_file = os.path.join(target_dir, filename)
+            shutil.copy(source_file, target_file)
+
+        # Clean up cloned repository
+        shutil.rmtree(source_dir)
+
+# Setup configuration
 setup(
     name='openpose-converter',
     version='0.0.1',
     packages=find_packages(),
     package_data={'openpose': ['*']},
-    scripts=['canny-maker-recursive.py', 'openpose-converter.py', 'resize768.py'],
-    install_requires=[
-        'openpose-library',
-    ],
-    dependency_links=[
-        'https://github.com/tranzmatt/openpose-library/archive/master.zip#egg=dependency-package',
-    ],
+    scripts=['canny-maker-recursive.py', 'openpose-converter.py', 'resize768.py', 'cropAndResize768.py', 'cropAndResizeFixed768.py' ],
+    install_requires=[],
+    cmdclass={
+        'install': CustomInstallCommand,
+    }
 )
 

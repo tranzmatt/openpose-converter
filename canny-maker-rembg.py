9fdb0dcb960205b48ae9e27bfe5a3145c1b0c47f
Matthew Clark <linux.matthew@gmail.com>
2023-07-03 23:28:15 -0400

    Force background of rembg to 255,255,255,1.  Otherwise background info was lingering and would mess up the canny

--- canny-maker-rembg.py
+++ canny-maker-rembg.py
@@ -3,11 +3,13 @@ import cv2
 import os
 import sys
 import fnmatch
-from rembg import remove
+from rembg import remove, new_session
 import numpy as np
 
 # set the default image pattern
 image_pattern = "*.768.jpg"
+
+session = new_session()
 
 # check if a command-line argument is provided
 if len(sys.argv) > 1:
@@ -30,35 +32,35 @@ def process_files(directory):
                 input_image = cv2.imread(image_file)
 
                 # isolate the foreground using rembg
-                output_image = remove(input_image)
-                rembg_output_filename = f"{filename}.rembg.png"
-                cv2.imwrite(rembg_output_filename, output_image)
-
-                # convert to grayscale
-                gray = cv2.cvtColor(output_image, cv2.COLOR_BGR2GRAY)
+                #rembg_image_tmp = remove(input_image, session=session, bgcolor=(255,255,255,1))
+                #rembg_image = remove(rembg_image_tmp, session=session)
+                rembg_image = remove(input_image, session=session, bgcolor=(255,255,255,1))
+                rembg_image_filename = f"{filename}.rembg.png"
+                cv2.imwrite(rembg_image_filename, rembg_image)
 
                 # set the low and high thresholds
-                low_threshold = 50
-                high_threshold = 150
+                low_threshold = 100
+                high_threshold = 200
 
                 # perform canny edge detection
-                canny_output = cv2.Canny(gray, low_threshold, high_threshold)
+                #canny_image = cv2.Canny(gray_image, low_threshold, high_threshold)
+                canny_image = cv2.Canny(rembg_image, low_threshold, high_threshold)
+                canny_image_filename = f"{filename}.rembg.canny_{low_threshold}_{high_threshold}.png"
+                cv2.imwrite(canny_image_filename, canny_image)
 
                 # invert the colors of the canny output
-                inverted_output = 255 - canny_output
+                inverted_image = 255 - canny_image
 
                 # apply binary thresholding using Otsu's method
-                _, threshold_output = cv2.threshold(canny_output, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+                _, threshold_image = cv2.threshold(canny_image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 
                 # construct the output filenames with the appropriate suffixes
-                canny_output_filename = f"{filename}.rembg.canny_{low_threshold}_{high_threshold}.png"
-                threshold_output_filename = f"{filename}.rembg.threshold.png"
-                inverted_output_filename = f"{filename}.rembg.threshold_inverted.png"
+                #threshold_image_filename = f"{filename}.rembg.threshold.png"
+                #inverted_image_filename = f"{filename}.rembg.threshold_inverted.png"
 
                 # save the output images
-                cv2.imwrite(canny_output_filename, canny_output)
-                cv2.imwrite(threshold_output_filename, threshold_output)
-                cv2.imwrite(inverted_output_filename, inverted_output)
+                #cv2.imwrite(threshold_image_filename, threshold_image)
+                #cv2.imwrite(inverted_image_filename, inverted_image)
         elif entry.is_dir():
             process_files(entry.path)  # recurse into subdirectories
 

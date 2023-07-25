8475a99f73bcef98a5bb44cbd9645da63856b727
Matthew Clark <linux.matthew@gmail.com>
2023-07-25 14:51:46 -0400

    Check for input image, adjust scaling of outputs

--- edge-detector.py
+++ edge-detector.py
@@ -8,6 +8,10 @@ import os
 import glob
 from multiprocessing import Pool
 import torch
+
+from PIL import Image
+import torchvision.transforms as transforms
+
 import cv2
 try:
     import kornia as K
@@ -23,14 +27,17 @@ def edge_detection(filepath, detectors):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    #img = cv2.imread(filepath)
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB
-    #img: Tensor = torch.from_numpy(img).permute(2, 0, 1).float() / 255.0  # permute dimensions and normalize
-    #img = img[None].to(device)
+    # Add file existence check
+    if not os.path.isfile(filepath):
+        print(f"File {filepath} does not exist.")
+        return
 
     img = K.io.load_image(filepath, K.io.ImageLoadType.RGB32).to(device)
     img = img[None]
     x_gray = K.color.rgb_to_grayscale(img)
+    x_gray_pil = transforms.ToPILImage()(x_gray.squeeze(0).cpu())
+    gray_filepath = filepath.rsplit('.', 1)[0] + '.' + 'gray' + '.' + filepath.rsplit('.', 1)[1]
+    x_gray_pil.save(gray_filepath)
 
     for detector in detectors:
         if detector == 'dx':
@@ -55,20 +62,33 @@ def edge_detection(filepath, detectors):
 
         elif detector == 'sobel':
             x_sobel = K.filters.sobel(x_gray)
-            output = K.utils.tensor_to_image(1. - x_sobel)
+            print(f"Sobel filter output min: {x_sobel.min()}, max: {x_sobel.max()}")
+            x_sobel = (x_sobel - x_sobel.min()) / (x_sobel.max() - x_sobel.min())
+            output = K.utils.tensor_to_image(x_sobel)
 
         elif detector == 'laplacian':
             x_laplacian = K.filters.laplacian(x_gray, kernel_size=5)
-            output = K.utils.tensor_to_image(1. - x_laplacian.clamp(0., 1.))
+            print(f"Laplacian filter output min: {x_laplacian.min()}, max: {x_laplacian.max()}")
+            x_laplacian = (x_laplacian - x_laplacian.min()) / (x_laplacian.max() - x_laplacian.min())
+            output = K.utils.tensor_to_image(x_laplacian)
+
+        elif detector == 'icanny':
+            x_icanny = K.filters.canny(x_gray)[0]
+            output = K.utils.tensor_to_image(x_icanny.clamp(0., 1.0))  # No inversion here.
 
         elif detector == 'canny':
             x_canny = K.filters.canny(x_gray)[0]
             output = K.utils.tensor_to_image(1. - x_canny.clamp(0., 1.0))
 
-        output_filepath = filepath.rsplit('.', 1)[0] + '_' + detector + '.' + filepath.rsplit('.', 1)[1]
-        #output.save(output_filepath)
-        cv2.imwrite(output_filepath, cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
+        output_filepath = filepath.rsplit('.', 1)[0] + '.' + detector + '.' + filepath.rsplit('.', 1)[1]
 
+        output = output.astype('float32')  # Make sure the output is float for the calculations
+        output = output - output.min()  # shift values to be >= 0
+        output = output / output.max()  # normalize to range [0, 1]
+        output = (output * 255).astype('uint8')  # scale to [0, 255]
+
+        print(f"Scaled output min: {output.min()}, max: {output.max()}")
+        cv2.imwrite(output_filepath, cv2.cvtColor(output, cv2.COLOR_RGB2BGR))
 
 def process_file(args):
     """
@@ -82,7 +102,7 @@ def split_string(s):
     """
     Helper function to split input string into list by commas and check the inputs are valid.
     """
-    valid_choices = ["dx", "dy", "d2x", "d2y", "sobel", "laplacian", "canny"]
+    valid_choices = ["dx", "dy", "d2x", "d2y", "sobel", "laplacian", "canny", "icanny"]
     input_list = s.split(',')
     
     # Check that each input is a valid choice

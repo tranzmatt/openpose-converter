849852fb2cbe6fa99c72c411eeb1d40343457f3a
Matthew Clark <linux.matthew@gmail.com>
2023-08-10 18:09:37 -0400

    Clean up pylint

--- rembg-maker.py
+++ rembg-maker.py
@@ -2,26 +2,48 @@
 
 import cv2
 import os
-import sys
 import fnmatch
+import argparse
 from rembg import remove, new_session
-import numpy as np
-
-# set the default image patterns
-image_patterns = ["*.jpg", "*.png"]
 
 session = new_session()
 
-# check if a command-line argument is provided
-if len(sys.argv) > 1:
-    image_patterns = [sys.argv[1]]
+DETECTOR_NAMES = ["canny", "hed", "mlsd", "midas", "openpose", "uniformer"]
+
+
+# get the images files
+def get_image_files(input_path, filters=None):
+    valid_extensions = [".jpg", ".JPG", ".png", ".PNG"]
+    if os.path.isdir(input_path):
+        image_files = []
+        for ext in valid_extensions:
+            image_files.extend(glob.glob(os.path.join(input_path, '**', '*' + ext), recursive=True))
+
+        # Filter out images with detector names
+        image_files = [img for img in image_files if not any(detector in img for detector in DETECTOR_NAMES)]
+
+        # Apply additional filters if provided
+        if filters:
+            filtered_files = []
+            for pattern in filters:
+                filtered_files.extend(fnmatch.filter(image_files, pattern))
+            image_files = filtered_files
+
+    elif os.path.isfile(input_path) and any(input_path.endswith(ext) for ext in valid_extensions):
+        image_files = [input_path]
+    else:
+        image_files = []
+
+    return image_files
+
 
 # recursive function to process files in a directory
-def process_files(directory):
+def process_files(directory, image_patterns):
     for entry in os.scandir(directory):
         if entry.is_file():
             file_lower = entry.name.lower()
-            if any(fnmatch.fnmatchcase(file_lower, pattern) for pattern in image_patterns) and "rembg" not in file_lower:
+            if any(fnmatch.fnmatchcase(file_lower, pattern) for pattern in image_patterns) \
+                    and "rembg" not in file_lower:
                 image_file = entry.path
                 print(f"Processing {image_file}")
                 filename, ext = os.path.splitext(image_file)
@@ -30,13 +52,21 @@ def process_files(directory):
                 input_image = cv2.imread(image_file)
 
                 # isolate the foreground using rembg
-                rembg_image = remove(input_image, session=session, bgcolor=(255,255,255,1))
+                rembg_image = remove(input_image, session=session, bgcolor=(255, 255, 255, 1))
                 rembg_image_filename = f"{filename}.rembg.png"
                 cv2.imwrite(rembg_image_filename, rembg_image)
 
         elif entry.is_dir():
-            process_files(entry.path)  # recurse into subdirectories
+            process_files(entry.path, image_patterns)  # recurse into subdirectories
 
-# start processing files in the current directory and its subdirectories
-process_files('.')
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process images in a specified directory.")
+    parser.add_argument('-p', '--path', default='.', help="Path to the directory to process.")
+    parser.add_argument('-f', '--filter', nargs='+', default=["*.jpg", "*.png"],
+                        help="Image filters (e.g. *.jpg *.png).")
+
+    args = parser.parse_args()
+
+    # start processing files in the specified directory and its subdirectories
+    process_files(args.path, args.filter)
